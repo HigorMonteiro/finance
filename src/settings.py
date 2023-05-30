b36139ececc2fed62881b00d7b7315e2ae8a964e
higorvinicius <higorvmonteiro@gmail.com>
2023-05-30 09:14:17 -0300

    health

--- src/settings.py
+++ src/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'health_check',
+    'health_check.db',
+
 ]
 
 MIDDLEWARE = [
--- src/urls.py
+++ src/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import health_check.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/app', include(health_check.urls)),
 ]
