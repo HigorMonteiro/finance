2bee4d5c0b00229d30908a5e7400c4ec3bd2fb4c
higorvinicius <higorvmonteiro@gmail.com>
2023-05-30 14:20:07 -0300

    create models for register

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(CreditCard)
+admin.site.register(Category)
+admin.site.register(BillCard)
+admin.site.register(Bill)
+admin.site.register(Income)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
 # Create your models here.
+
+class CreditCard(models.Model):
+    card = models.CharField(max_length=200)
+    flag = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.card
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class BillCard(models.Model):
+    cred_card= models.ForeignKey(CreditCard,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,)
+    category= models.ForeignKey(Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,)
+    place = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    sale_date = models.DateTimeField(auto_now_add=True)
+    is_recurrent = models.BooleanField(default=False)
+    parcel = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.place} - R$: {self.value}" 
+
+class Bill(models.Model):
+    category= models.ForeignKey(Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,)
+    place = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    sale_date = models.DateTimeField(auto_now_add=True)
+    is_recurrent = models.BooleanField(default=False)
+    parcel = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.place} - R$: {self.value}" 
+
+
+class Income(models.Model):
+    category= models.ForeignKey(Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    sale_date = models.DateTimeField(auto_now_add=True)
+    is_recurrent = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.category} - R$: {self.value}" 
